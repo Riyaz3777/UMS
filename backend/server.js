947f8d22d679d31e4375be24f82b87cc4fe1c9a3
Riyaz3777 <8019454867srz@gmail.com>
2025-08-27 15:37:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,12 @@ app.use(express.static(path.join(__dirname, "../public")));
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const path = require("path");
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
